37abdd3feda90d4a8bc6d34f0edb18e85ca7dffb
Noel Varghese <noelvarghese2006@gmail.com>
2024-10-12 16:58:48 -0500

    Added pytrends

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-import httpx
-import json
-import pandas as pd
-import time
+from pytrends.request import TrendReq
+import matplotlib.pyplot as plt
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 
-# Set up Chrome options to run in headless mode
-chrome_options = Options()
-# chrome_options.add_argument("--headless")
-# This is temporary
-chrome_options.add_argument("--incognito")
-# Pass the Service object to the Chrome WebDriver
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://trends.google.com")
-time.sleep(10)
-driver.maximize_window()
-# driver.get("https://google.com")
-search_bar = driver.find_element(By.CSS_SELECTOR, '.Fgl6fe-fmcmS-wGMbrd')
-info = input("Enter something you would like to know: ")
-search_bar.clear()
-search_bar.send_keys(info)
-explore_button = driver.find_element(By.CSS_SELECTOR, '.Qt4Qjb')
-explore_button.click()
-time.sleep(1000)
+pytrends = TrendReq(hl="en-US", tz = 360)
+count = int(input("Enter how many items you wish to compare: "))
+kw_list = []
+for i in range(count):
+    kw_list.append(input("Enter keyword " + str(i+1) + ": "))
+pytrends.build_payload(kw_list, timeframe='today 12-m')
+
+data = pytrends.interest_over_time()
+print(data)
+data.plot(figsize=(10,6))
+plt.title('Keyword Popularity Over Time')
+plt.xlabel('Date')
+plt.ylabel('Interest')
+plt.show()
